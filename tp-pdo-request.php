d11973f0f65c68a6a2d6d7b731a3434edfba0125
Elysio Martins <77900181+Refaltor77@users.noreply.github.com>
2023-10-09 23:37:20 +0200

    Add files via upload

---
+++ tp-pdo-request.php
@@ -0,0 +1,24 @@
+<?php
+
+
+# Créez une table nommée "users_" suivi de votre pseudo ou de votre nom.
+# données de la table : name, forename, age, postal_code
+
+
+# ---
+
+
+
+$name = "Doe";
+$forename = "John";
+$age = 36;
+$postalCode = 77160; // seine et marne
+
+
+# Effectuez une requête préparée pour insérer
+# les données sauvegardées dans les variables situées juste au-dessus.
+
+
+# ---
+
+# Veuillez attendre la validation de votre formateur.
