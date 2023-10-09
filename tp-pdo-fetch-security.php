2dfb42bedde562654d462e7b67903c6ff36ceaad
Elysio Martins <77900181+Refaltor77@users.noreply.github.com>
2023-10-10 00:04:27 +0200

    Add files via upload

---
+++ tp-pdo-fetch-security.php
@@ -0,0 +1,12 @@
+<?php
+
+
+# Effectuez une requête "SELECT *" sur la table "users_list"
+# et ensuite utilisez une boucle pour extraire les données de
+# chaque ligne et les stocker dans un tableau associatif.
+# Faite une structure de code afin de gérér les erreurs.
+$arrayAssoc = [];
+
+
+
+# ---
